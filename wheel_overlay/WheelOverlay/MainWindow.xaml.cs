86177fe22efcf633d12b5e5361fb1d7cde18d642
Gavin Adams <me@gavinadams.org>
2025-11-29 20:27:43 -0500

    Implement Wheel Overlay with DirectInput support for BavarianSimTec Alpha

--- wheel_overlay/WheelOverlay/MainWindow.xaml.cs
+++ wheel_overlay/WheelOverlay/MainWindow.xaml.cs
@@ -1,28 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
+using WheelOverlay.Services;
 
 namespace WheelOverlay
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly InputService _inputService;
+        private readonly string[] _displayItems = { "DASH", "TC2", "MAP", "FUEL", "BRGT", "VOL", "BOX", "DIFF" };
+
         public MainWindow()
         {
             InitializeComponent();
+            _inputService = new InputService();
+            _inputService.RotaryPositionChanged += OnRotaryPositionChanged;
+            
+            Loaded += MainWindow_Loaded;
+            Closed += MainWindow_Closed;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _inputService.Start();
+            
+            MakeWindowTransparent();
+        }
+
+        private void MakeWindowTransparent()
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW);
+        }
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+        private const int WS_EX_TOOLWINDOW = 0x00000080; // Hides from Alt-Tab
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+
+        private void MainWindow_Closed(object? sender, EventArgs e)
+        {
+            _inputService.Stop();
+            _inputService.Dispose();
+        }
+
+        private void OnRotaryPositionChanged(object? sender, int position)
+        {
+            if (position >= 0 && position < _displayItems.Length)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    StatusText.Text = _displayItems[position];
+                });
+            }
         }
     }
 }
